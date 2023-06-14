02acffcd3d396f47533de3c2bbe63f284c718ebb
sferrante <sferrante0604@gmail.com>
2023-06-14 14:33:46 -0400

    Add files via upload

--- WIC_Pheno_Formulae.py
+++ WIC_Pheno_Formulae.py
@@ -33,27 +33,25 @@ SinSquaredAlpha = 0.1;  GeVm2toAttobarn = 0.3894*(10**15);
 σCOEFF = ( (GeVm2toAttobarn*ce*(SinSquaredAlpha*gZ)**2)/(8*np.pi*(np.pi)**2) ) * (1/4); ## 1/4 to average over spins 
 
 def σμ(sqrtS, μ1, μ2):
-    return (( (sqrtS**4 - 2*sqrtS**2*(μ1**2 + μ2**2) + (μ1**2 - μ2**2)**2 ) / (4*sqrtS**2)  )**(3/2)) /\
-           (sqrtS*(sqrtS**2 - mZ**2)**2)
+    den = (sqrtS*(sqrtS**2 - mZ**2)**2)
+    return ( ( (sqrtS**4 - 2*sqrtS**2*(μ1**2 + μ2**2) + (μ1**2 - μ2**2)**2) / (4*sqrtS**2) )**(3/2) ) / den
 
-def d2σ_dμ1dμ1p(μ1, μ1p, sqrtS, μ0, μϼ):
+def d2σ_dμ1dμ1p(X, parameters):
+    sqrtS = parameters[0]; μ0 = parameters[1]; μϼ = parameters[2];
+    μ1   = X[0]
+    μ1p  = X[1]
     if μ0 < μ1 < sqrtS - μ0 and μ0 < μ1p < sqrtS - μ1: 
         return σCOEFF*μϼ(μ1)*μϼ(μ1p)*σμ(sqrtS, μ1, μ1p)
     else:
         return 0
-def d2σ_dμ1dμ1p_v(X, sqrtS, μ0, μϼ):
-    μ1   = X[0]
-    μ1p  = X[1]
-    return d2σ_dμ1dμ1p(μ1, μ1p, sqrtS, μ0, μϼ) 
+
 
 ################
 ##  dσ_dcosθ  ## 
 ################
-def dσ_dcosθ(x):
-    return 1-x**2
-def dσ_dcosθ_v(X):
+def dσ_dcosθ(X, parameters):
     x   = X[0]
-    return dσ_dcosθ(x) 
+    return 1-x**2
 
     
 ###########
@@ -72,17 +70,43 @@ def σ(sqrtS, μ0, μϼ):
     return quad(dσdμ, μ0, sqrtS-μ0, args=(μ0,sqrtS,μϼ))[0]
 
 
+##########################################
+## d2σ_dμ1dμ1p with Weizsacker-Williams ## 
+##########################################
+σCOEFF = ( (GeVm2toAttobarn*ce*(SinSquaredAlpha*gZ)**2)/(8*np.pi*(np.pi)**2) ) * (1/4); ## 1/4 to average over spins 
+
+def fww(z, sqrtS):
+    me=0.005; 
+    return ((ceγ**2)/(8*np.pi**2)) * ((1 + (1-z)**2)/(z)) * np.log(10)
+#             * np.log((sqrtS/me)**2)
+            
+def σμ(sqrtS, μ1, μ2):
+    den = (sqrtS*(sqrtS**2 - mZ**2)**2)
+    return ( ( (sqrtS**4 - 2*sqrtS**2*(μ1**2 + μ2**2) + (μ1**2 - μ2**2)**2) / (4*sqrtS**2) )**(3/2) ) / den
+
+
+def d2σww_dμ1dμ1p(X, parameters):
+    sqrtS = parameters[0]; μ0 = parameters[1]; μϼ = parameters[2]; 
+    μ1   = X[0]
+    μ1p  = X[1]
+    z    = X[2]
+    ## 
+    if μ0 < μ1 < sqrtS*np.sqrt((1-z)) - μ0 and μ0 < μ1p < sqrtS*np.sqrt((1-z)) - μ1: 
+        return σCOEFF*μϼ(μ1)*μϼ(μ1p)*fww(z, sqrtS)*σμ(sqrtS*np.sqrt((1-z)), μ1, μ1p)
+    else:
+        return 0
+
+
 ##########################
 ## d4σ_deγdcosθγdμ1dμ1p ##   ( KK production w/ Initial state radiation ) 
 ##########################
-
 σISR_COEFF = GeVm2toAttobarn*ce*((ceγ*SinSquaredAlpha*gZ)/(2))**2 * (1/4); ## 1/4 to average over spins 
 
 def Ratio(μ1,sqrtS):
     return (2*μ1/sqrtS)**2
 def R2(μ1p,sqrtS):
     return (2*μ1p/sqrtS)**2
-def dR(μ1, μ1p,sqrtS):
+def dR(μ1, μ1p, sqrtS):
     return (2*μ1/sqrtS)**2 - (2*μ1p/sqrtS)**2
 def σISRμ(μ1, μ1p, eγ, cosθγ, sqrtS):
     R1_=Ratio(μ1,sqrtS)
@@ -92,36 +116,33 @@ def σISRμ(μ1, μ1p, eγ, cosθγ, sqrtS):
     xγ = eγ/(sqrtS/2)
     θγ = np.arccos(cosθγ)
     num1  = 2*np.pi**2*(sqrtS**2 / 4)*(1/(np.sin(θγ)**2))*(8*(xγ-1)*ΣR_ + ΔR_**2 + 16*(xγ-1)**2)**(3/2)
-    num2  = ((xγ-1)*(xγ+3)*np.cos(2*θγ) + xγ*(3*xγ-10) + 11)
+#     num2  = ((xγ-1)*(xγ+3)*np.cos(2*θγ) + xγ*(3*xγ-10) + 11)
+    num2  = ((xγ*(xγ+2)-2)*np.cos(2*θγ) + xγ*(3*xγ-10) + 10)
     denom = (3*(xγ-1)**2*xγ*(sqrtS**2*(xγ-1)+mZ**2)**2)
-    return num1*num2/denom
+    return (num1*num2)/denom
 
-             
-                                          
-def d4σ_deγdcosθγdμ1dμ1p(μ1, μ1p, eγ, cosθγ, sqrtS, μ0, μϼ):
+
+def d4σ_deγdcosθγdμ1dμ1p(X, parameters):
+    sqrtS = parameters[0]; μ0 = parameters[1]; μϼ = parameters[2]; θcut = parameters[3]; xγcut = parameters[4];
+    μ1    = X[0]
+    μ1p   = X[1]
+    eγ    = X[2]
+    cosθγ = X[3]
+    ## 
     R1_=Ratio(μ1,sqrtS)
     R2_=Ratio(μ1p,sqrtS)
     ΔR_=R1_-R2_
     ΣR_=R1_+R2_
     e = (sqrtS/2)
     xγ  = eγ/e
-    θc=0.1
-    xγc = 0.05
-    if μ1p < sqrtS-μ1-e*xγc and\
-       μ1  < sqrtS-μ0-e*xγc and\
-       xγ  > xγc and\
+    if μ1p < sqrtS-μ1-e*xγcut and\
+       μ1  < sqrtS-μ0-e*xγcut and\
+       xγ  > xγcut and\
        xγ < 1-(ΣR_/4) - (np.sqrt(R1_*R2_)/2) and\
-       -1+θc < cosθγ < 1-θc:
-        return σISR_COEFF*(μϼ(μ1)*μϼ(μ1p)/(np.pi**2))*σISRμ(μ1, μ1p, eγ, cosθγ, sqrtS)/(64*(2*np.pi)**5)
+       -1+θcut < cosθγ < 1-θcut:
+        return σISR_COEFF*(μϼ(μ1)*μϼ(μ1p)/(np.pi**2))*σISRμ(μ1, μ1p, eγ, cosθγ, sqrtS)/(64*(2*np.pi)**5 * (sqrtS/2))
     else: 
         return 0
-def d4σ_deγdcosθγdμ1dμ1p_v(X, sqrtS, μ0, μϼ):
-    μ1    = X[0]
-    μ1p   = X[1]
-    eγ    = X[2]
-    cosθγ = X[3]
-    return d4σ_deγdcosθγdμ1dμ1p(μ1, μ1p, eγ, cosθγ, sqrtS, μ0, μϼ)
-
 
 
 ###########################################################################################################################
@@ -132,29 +153,45 @@ def R(μ1,μ2):
 def R0(μ1,μ0):
     return (μ0/μ1)**2
 def MSquared3body(x3, x4, μ1, μ2): 
-    return (2*(μ1**4)*(cf*(SinSquaredAlpha*gZ)**2)*(1 - R(μ1, μ2) + x3*x4 - x3 - x4)) /\
+    return ((μ1**4)*(cf*(SinSquaredAlpha*gZ)**2)*(1 - R(μ1, μ2) + x3*x4 - x3 - x4)) /\
            (np.abs((μ1**2)*(x3 + x4 + R(μ1, μ2) - 1) - mZ**2 + mZ*ZRate*1j)**2) 
 def MSquared2body(μ1, μ2): 
     return (((SinSquaredAlpha*gZ)/2)**2)*(((μ1**2 - μ2**2)/mZ)**2 - 2*(μ1**2 + μ2**2) + mZ**2)
 
-###########
-## dΓ_dμ ## 
-###########
-
+##########################
+## dΓ_dμ ## (continuum version) 
+##########################
 ######### 3 body #########
-def d3Γ3_dμ2dx3dx4(μ2, x3, x4, μ1, μ0, μϼ, Δm2):
+def d3Γ3_dμ2dx3dx4(X, parameters):
+    μ1 = parameters[0]; μ0 = parameters[1]; μϼ = parameters[2]; Δm2 = parameters[3]
+    μ2 = X[0]
+    x3 = X[1]
+    x4 = X[2]
     if μ0 <= μ2 < μ1 and\
        1 - (x3/2) - (x4/2) >= np.sqrt(R(μ1,μ2)) and\
        x3 + x4         >= 1 - R(μ1,μ2) and\
        x3 + x4 - x3*x4 <= 1 - R(μ1,μ2):
-        return ((μϼ(μ1)/μ1) * Δm2)*(μ1/(256*np.pi**4))*μϼ(μ2)*MSquared3body(x3, x4, μ1, μ2)
+        return ((μϼ(μ1)/μ1) * Δm2)*(μ1/(128*np.pi**4))*μϼ(μ2)*MSquared3body(x3, x4, μ1, μ2)
     else:
         return 0
-def d3Γ3_dμ2dx3dx4_v(X, μ1, μ0, μϼ, Δm2):
-    μ2 = X[0]
-    x3 = X[1]
-    x4 = X[2]
-    return d3Γ3_dμ2dx3dx4(μ2, x3, x4, μ1, μ0, μϼ, Δm2)
+
+
+##########################
+## dΓ_dx3dx4 ## (discrete version)
+##########################
+######### 3 body #########
+def d2Γ3_dx3dx4(X, parameters):
+    n = parameters[0]; m = parameters[1]; μs = parameters[2]; ψRs = parameters[3]; c = parameters[4];
+    x3 = X[0]
+    x4 = X[1]
+    μ1=μs[n]; μ2=μs[m]; 
+    if 1 - (x3/2) - (x4/2) >= np.sqrt(R(μ1,μ2)) and\
+       x3 + x4         >= 1 - R(μ1,μ2) and\
+       x3 + x4 - x3*x4 <= 1 - R(μ1,μ2):
+        return (μ1/(128*np.pi**3))*MSquared3body(x3, x4, μ1, μ2)*ψRs[n]*ψRs[m]/(c**2)
+    else:
+        return 0
+
 
 
 ######### 2 body #########
